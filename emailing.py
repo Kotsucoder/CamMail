c47e823a5ae8d0df93751f41d158f6a41c8906a5
Marcus Kotsu <whoami@kotsu.red>
2024-08-11 12:45:02 -0700

    Fixing several issues with final program.

--- emailing.py
+++ emailing.py
@@ -1,29 +1,54 @@
 import os
 import smtplib
 import imghdr
+import glob
 from email.message import EmailMessage
 
 PASSWORD = os.getenv("PASSWORD")
-SENDER = "pykotsumail@gmail.com"
-RECIEVER = "pykotsumail@proton.me"
+SENDER = "ENTER YOUR OWN EMAIL"
+RECIEVER = "ENTER YOUR OWN EMAIL"
+still_sending = False
+cleaning = False
+
+def clean_folder():
+    global cleaning
+    if not cleaning:
+        print("clean_folder function started")
+        cleaning = True
+        images = sorted(glob.glob("images/*.png"))[1:]
+        for image in images:
+            os.remove(image)
+        print("clean_folder function ended")
+        cleaning = False
+        return True
+    else:
+        print("Hold on!")
+        return False
 
 def send_email(image_path):
-    print("send_email function started")
-    email_message = EmailMessage()
-    email_message["Subject"] = "New customer showed up!"
-    email_message.set_content("Hey, we just saw a new customer!")
+    global still_sending
+    if not still_sending:
+        print("send_email function started")
+        still_sending = True
+        email_message = EmailMessage()
+        email_message["Subject"] = "New customer showed up!"
+        email_message.set_content("Hey, we just saw a new customer!")
 
-    with open(image_path, "rb") as file:
-        content = file.read()
-    email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
+        with open(image_path, "rb") as file:
+            content = file.read()
+        email_message.add_attachment(content, maintype="image", subtype=imghdr.what(None, content))
 
-    gmail = smtplib.SMTP("smtp.gmail.com", 587)
-    gmail.ehlo()
-    gmail.starttls()
-    gmail.login(SENDER, PASSWORD)
-    gmail.sendmail(SENDER, RECIEVER, email_message.as_string())
-    gmail.quit()
-    print("send_email function ended")
+        gmail = smtplib.SMTP("smtp.gmail.com", 587)
+        gmail.ehlo()
+        gmail.starttls()
+        gmail.login(SENDER, PASSWORD)
+        gmail.sendmail(SENDER, RECIEVER, email_message.as_string())
+        gmail.quit()
+        print("send_email function ended")
+        still_sending = False
+        clean_folder()
+    else:
+        print("Global Variable Worked!")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -1,72 +1,80 @@
 import cv2
 import time
 import glob
-import os
 from emailing import send_email
+from emailing import clean_folder
 from threading import Thread
+
+x = 0
+y = 0
+w = 0
+h = 0
+
+def feed_monitor():
+    global x
+    global y
+    global w
+    global h
+    first_frame = None
+    status_list = []
+    count = 1
+    threaded_video = cv2.VideoCapture(0)
+    time.sleep(5)
+
+    while True:
+        status = 0
+        check, threaded_frame = threaded_video.read()
+
+        gray_frame = cv2.cvtColor(threaded_frame, cv2.COLOR_BGR2GRAY)
+        gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
+
+        if first_frame is None:
+            first_frame = gray_frame_gau
+
+        delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
+
+        thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
+        dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
+        # cv2.imshow("My video", dil_frame)
+
+        contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+
+        for contour in contours:
+            if cv2.contourArea(contour) < 5000:
+                continue
+            x, y, w, h = cv2.boundingRect(contour)
+            rectangle = cv2.rectangle(threaded_frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+            if rectangle.any():
+                status = 1
+                cv2.imwrite(f"images/{count}.png", threaded_frame)
+                count = count + 1
+                all_images = glob.glob("images/*.png")
+                index = int(len(all_images) / 2)
+                image_with_object = all_images[index]
+
+
+        
+        status_list.append(status)
+        status_list = status_list[-2:]
+
+        if status_list[0] == 1 and status_list[1] == 0:
+            email_thread = Thread(target=send_email, args=(image_with_object, ))
+            email_thread.daemon = True
+            email_thread.start()
+
+
+
+feed_thread = Thread(target=feed_monitor)
+feed_thread.daemon = True
+feed_thread.start()
 
 video = cv2.VideoCapture(0)
 time.sleep(5)
 
-first_frame = None
-status_list = []
-count = 1
-
-
-def clean_folder():
-    print("clean_folder function started")
-    images = sorted(glob.glob("images/*.png"))[1:]
-    for image in images:
-        os.remove(image)
-    print("clean_folder function ended")
-
 while True:
-    status = 0
     check, frame = video.read()
-
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    gray_frame_gau = cv2.GaussianBlur(gray_frame, (21, 21), 0)
-
-    if first_frame is None:
-        first_frame = gray_frame_gau
-
-    delta_frame = cv2.absdiff(first_frame, gray_frame_gau)
-
-    thresh_frame = cv2.threshold(delta_frame, 60, 255, cv2.THRESH_BINARY)[1]
-    dil_frame = cv2.dilate(thresh_frame, None, iterations=2)
-    cv2.imshow("My video", dil_frame)
-
-    contours, check = cv2.findContours(dil_frame, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-
-    for contour in contours:
-        if cv2.contourArea(contour) < 5000:
-            continue
-        x, y, w, h = cv2.boundingRect(contour)
-        rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
-        if rectangle.any():
-            status = 1
-            cv2.imwrite(f"images/{count}.png", frame)
-            count = count + 1
-            all_images = glob.glob("images/*.png")
-            index = int(len(all_images) / 2)
-            image_with_object = all_images[index]
-
-
-    
-    status_list.append(status)
-    status_list = status_list[-2:]
-
-    if status_list[0] == 1 and status_list[1] == 0:
-        email_thread = Thread(target=send_email, args=(image_with_object, ))
-        email_thread.daemon = True
-        clean_thread = Thread(target=clean_folder)
-        clean_thread.daemon = True
-
-        email_thread.start()
-        
-
-
-    print(status_list)
+    rectangle = cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 3)
+    # print(status_list)
 
     cv2.imshow("Video", frame)
     key = cv2.waitKey(1)
@@ -75,5 +83,7 @@ while True:
         break
 
 video.release()
-
-clean_thread.start()
+try_cleaning = clean_folder()
+while not try_cleaning:
+    time.sleep(1)
+    try_cleaning = clean_folder()
