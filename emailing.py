80a3112e3d599a151e627bd22fc7f718314ddc81
Marcus Kotsu <whoami@kotsu.red>
2024-08-11 11:38:20 -0700

    Threading in Python

--- emailing.py
+++ emailing.py
@@ -8,6 +8,7 @@ SENDER = "pykotsumail@gmail.com"
 RECIEVER = "pykotsumail@proton.me"
 
 def send_email(image_path):
+    print("send_email function started")
     email_message = EmailMessage()
     email_message["Subject"] = "New customer showed up!"
     email_message.set_content("Hey, we just saw a new customer!")
@@ -22,6 +23,7 @@ def send_email(image_path):
     gmail.login(SENDER, PASSWORD)
     gmail.sendmail(SENDER, RECIEVER, email_message.as_string())
     gmail.quit()
+    print("send_email function ended")
 
 
 if __name__ == "__main__":
--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import time
 import glob
 import os
 from emailing import send_email
+from threading import Thread
 
 video = cv2.VideoCapture(0)
 time.sleep(5)
@@ -13,9 +14,11 @@ count = 1
 
 
 def clean_folder():
+    print("clean_folder function started")
     images = sorted(glob.glob("images/*.png"))[1:]
     for image in images:
         os.remove(image)
+    print("clean_folder function ended")
 
 while True:
     status = 0
@@ -54,8 +57,13 @@ while True:
     status_list = status_list[-2:]
 
     if status_list[0] == 1 and status_list[1] == 0:
-        send_email(image_with_object)
-        clean_folder()
+        email_thread = Thread(target=send_email, args=(image_with_object, ))
+        email_thread.daemon = True
+        clean_thread = Thread(target=clean_folder)
+        clean_thread.daemon = True
+
+        email_thread.start()
+        
 
 
     print(status_list)
@@ -67,3 +75,5 @@ while True:
         break
 
 video.release()
+
+clean_thread.start()
